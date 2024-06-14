584f0f1d67aa68107a2426a44bcb5281d753562b
bushra3112004 <pathanbushra006@gmail.com>
2024-06-14 14:35:40 -0700

    add theme

---
+++ src/configData.js
@@ -0,0 +1,10 @@
+{
+    const title= "Cosmetic product app"
+            const theme= {
+        "primaryColor": " #ffdd99 rgb(255, 221, 153) hsl(40, 100 %, 80 %) ",
+        "secondaryColor": "#ffbf80 rgb(255, 191, 128) hsl(30, 100%, 75 %) ",
+                "fontColor/text": "black",
+                "font style":"playfair display"
+    }
+}
+
