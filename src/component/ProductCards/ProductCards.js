f09f16a4e5720d95f1d8ed2d43440a7b75f00a66
Sdsurve <sdsurve55555@gmail.com>
2024-06-15 01:58:50 +0530

    product card

---
+++ src/component/ProductCards/ProductCards.js
@@ -0,0 +1,19 @@
+import './ProductCards.css'
+function ProductCards( {ProductImg,Title,Price}) {
+  return (
+    <>
+
+    <div className='ProductCard'>
+        <div className='image'>
+            <img height={'230px'} className='img' src={ProductImg}/>
+        </div>
+        <div className='info'>
+            <h3>{Title.substring(0,51)}...</h3>
+        </div>
+    </div>
+
+    </>
+  )
+}
+
+export default ProductCards
--- src/index.js
+++ src/index.js
@@ -2,24 +2,33 @@ import ReactDOM from "react-dom/client"
 import Home from "./views/Home/Home";
 import About from "./views/About/About";
 import Contact from "./views/Contact/Contact";
+import Product from "./views/Product/Product"
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
- const path = window.location.pathname
- if(path === "/")
+ const router = createBrowserRouter([
     {
-        root.render(<Home />)
-    }
-    else if (path === "/about")
-        {
-            root.render(<About />)
-        }
-        else if (path === "/contact")
-            {
-                root.render(<Contact />)
-            }
-            else{
-                root.render(<h1>404 Page Not Found</h1>)
-            }
+        path:'/',
+        element:<Home/>,
+    },
+    {
+        path:'/About',
+        element:<About/>,
+    },
+    {
+        path:'/Contact',
+        element:<Contact/>,
+    },
+    {
+        path:'/Product',
+        element:<Product/>,
+    },
+    {
+        path:'/',
+        element:<Home/>,
+    },
+ ])
+root.render(<RouterProvider router={router}/>)
