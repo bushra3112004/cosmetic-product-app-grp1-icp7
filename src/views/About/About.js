aeb078132d545a57642b517b092fa14178cc6154
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-06-14 15:53:50 +0530

    about page basic 2 section added

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import "./About.css"
+import bg1 from "./bg1.jpg"
+import nailPaints from "./about-image-2.jpg"
+import fruit from "./fruit.png"
+import floral from "./floral.png"
+import ocean from "./ocean.png"
 
 function About() {
   return (
-    <div>
-        about
-    </div>
+    <>
+        <div>
+            <div>
+                <img src={bg1} className="title-bg" />
+            </div>
+            <div className="cosmetic-types-data">
+                <div className="cosmetic-types-data-1">
+                    <img src={nailPaints} />
+                </div>
+                <div className="cosmetic-types-data-2">
+                    <div className="cosmetic-types-data-2-text">
+                        <h1>NATURAL ORGANIC COSMETICS PROMOTES REJUVENATION</h1>
+                        <p>Bleak cod cutthroat eel sargassum fish panga sucker beardfish lionfish batfish eulachon Raccoon butterfly fish tetra. Pejerrey combtooth blenny Sundaland Noodlefish pipefish zebra shark beaked salmon hillstream loach New Zealand smelt salmon false moray pelican eel stoneroller minnow.</p>
+                    </div>
+                    <div className="types-images">
+                        <div>
+                            <img src={fruit} className="type-img"/>
+                            <p className="img-text">Fresh Types</p>
+                        </div>
+                        <div>
+                            <img src={floral} className="type-img"/>
+                            <p className="img-text">Floral Types</p>
+                        </div>
+                        <div>
+                            <img src={ocean} className="type-img"/>
+                            <p className="img-text">Oceanic Types</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div >
+                <h1>Our Achievements</h1>
+            </div>
+        </div>
+    </>
   )
 }
 
