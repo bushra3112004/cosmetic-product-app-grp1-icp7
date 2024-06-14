441adae2b4adc6f946f8e8adb6e7d1960f2d9d94
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-06-14 15:59:43 +0530

    configurable heading and description color added in about page

--- src/configData.js
+++ src/configData.js
@@ -1,10 +1,9 @@
-{
-    const title= "Cosmetic product app"
-            const theme= {
-        "primaryColor": " #EAAA85",
-        "secondaryColor": "#F5E0D2 ",
-                "Heading": "#232323",
-                "description":"#616161"
+const title= "Cosmetic product app"
+const theme= {
+        primaryColor: "#EAAA85",
+        secondaryColor: "#F5E0D2 ",
+        Heading: "#232323",
+        description:"#616161"
     }
-}
 
+export {title,theme}
