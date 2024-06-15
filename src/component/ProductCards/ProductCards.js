0f55475b059da2fd1b69fc82f8dab64dbe32b823
Sdsurve <sdsurve55555@gmail.com>
2024-06-15 13:47:36 +0530

    add card

--- src/component/ProductCards/ProductCards.js
+++ src/component/ProductCards/ProductCards.js
@@ -8,6 +8,7 @@ function ProductCards( {ProductImg,Title,Price}) {
             <img height={'230px'} className='img' src={ProductImg}/>
         </div>
         <div className='info'>
+          <h3>{Title}</h3>
             <h3>{Title.substring(0,51)}...</h3>
         </div>
     </div>
