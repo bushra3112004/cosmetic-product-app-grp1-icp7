cbde26f4b7c5c56320983b307816f3869422c511
bushra3112004 <pathanbushra006@gmail.com>
2024-06-14 19:24:34 -0700

    add css

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -6,20 +6,21 @@ function login(){
    
       return(
           <form>
+            <h1 className="heading">Login Form</h1>
             <div className="main-container">
             <img src={ProfileImg} className="profile-img"/>
             <div className="container">
-              <label>Username:
+              <label for="uname"className="input-name"><b>Username:</b>
                 <br></br>
-                  <input type="text" />
+                <input type="text" placeholder="Enter Username" name="uname"  className="input-box"/>
               </label>
               <br></br>
-              <label>Password:
+              <label for="psw" className="input-name"><b>Password:</b>
                 <br></br>
-                <input type="password"/>
+                <input type="password" placeholder="Enter Password" name="psw" className="input-box"/>
               </label>
             <br></br>
-                      <button type="submit">Login</button>
+                      <button type="submit" className="submit-btn">Login</button>
                       <br></br>
                       <label>
                           <input type="checkbox" checked="checked" name="remember"/> Remember me
