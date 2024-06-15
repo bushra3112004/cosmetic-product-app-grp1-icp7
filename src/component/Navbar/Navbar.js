8aa4728393a1ed4604c68e0aa17cf8b39152105b
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-06-15 18:31:10 +0530

    navbar added

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <>
+     <div class="navbar-container">
+            <span class="nav-item">HOME</span>
+            <span class="nav-item">ABOUT</span>
+            <span class="nav-item">CONTACT</span>
+            <span class="nav-item">PRODUCT</span>
+            <span class="nav-item">REVIEW</span>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -11,25 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const router = createBrowserRouter([
     {
-<<<<<<< HEAD
-        root.render(<Home />)
-    }
-    else if (path === "/About")
-        {
-            root.render(<About />)
-        }
-        else if (path === "/Contact")
-            {
-                root.render(<Contact />)
-            }
-         else if(path === "/login")   
-            {
-                root.render(<Login/>)
-            }
-            else{
-                root.render(<h1>404 Page Not Found</h1>)
-            }
-=======
         path: "/",
         element: <Home />
     },
@@ -54,4 +35,3 @@ const router = createBrowserRouter([
 
 root.render(<RouterProvider router={router} />);
 export default index
->>>>>>> 9ba0cae539e873f0baef5c24e860b42956c2e7c3
