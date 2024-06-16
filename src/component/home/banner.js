eb3ad2e6c945c438a4ca9584938ce382800c754a
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-06-16 18:36:05 +0530

    home layout changed .

--- src/component/home/banner.js
+++ src/component/home/banner.js
@@ -4,7 +4,7 @@ import { theme } from "./../../configData";
 import Button from "./../../component/ButtonBox/ButtonBox"
 const banner = ()=>{
     return(<>
-        <div className=" container banner-container">
+        <div className="  banner-container">
                 <img src={mainbanner} className="background-img "/>
                < div className="description-container   ">
              <h1 style={{color: theme.primaryColor}} className="p-3 headingFont">Discover Your True Beauty </h1>
@@ -17,12 +17,5 @@ const banner = ()=>{
     
     )
     }
-    export default banner;
-   /* <div className="  m-2 pt-5 p-2 col-xs-12 ">
-    <h1 style={{color: theme.primaryColor}} className="p-3">Discover Your True Beauty </h1>
-        <h5 style={{color: theme.description,fontStyle: "italic"}} className="p-2">Popularized through customer relationships with some of the world’s most recognizable faces, the “brow revolution” she ignited has become a landmark</h5> 
-        <button className="btn-discover "> Discover</button>
-    </div>
-    <div className="col-xs-12 ">
-        <img src={mainbanner} className="background-img  rounded float-end" alt="background" />
-    </div>*/
+    export default banner
+   
