ef69aeb02161cfc224fd472109542305f56cbed8
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-06-16 11:26:09 +0530

    image added and appropriate description

--- src/component/TeamCard/TeamCard.js
+++ src/component/TeamCard/TeamCard.js
@@ -1,11 +1,12 @@
 import "./TeamCard.css"
+import { theme } from "../../configData"
 
 function TeamCard({TeamImage,TeamName,TeamPosition}) {
   return (
     <div className="team-card-container">
         <img src={TeamImage} className="team-image"/>
-        <span className="team-name-text">{TeamName}</span><br/>
-        <span className="team-position-text">{TeamPosition}</span>
+        <span className="team-name-text" style={{color: theme.Heading}} >{TeamName}</span><br/>
+        <span className="team-position-text" style={{color: theme.description}}>{TeamPosition}</span>
     </div>
   )
 }
