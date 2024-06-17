7e3b681dbba816a88d1042dfd951502a36f7ab02
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-06-17 17:58:45 +0530

    product images change

--- src/component/home/productContainer.js
+++ src/component/home/productContainer.js
@@ -7,16 +7,16 @@ const ProductContainer = ({ productImg, productName, productPrice, rating }) => 
   return (
     <div className="product-card">
       
-      <img src={productImg} alt={productName} />
+      <img src={productImg} alt={productName} className='productImg' />
       <div className="product-info">
-        <h2>{productName}</h2>
+        <h3>{productName}</h3>
         <p>{productPrice}</p>
         <div className="rating">
-          {rating}
+        <span className=''>Rating </span>  {rating}
         </div>
       </div>
     </div>
-  );
+  )
 };
 
 export default ProductContainer;
--- src/component/home/productList.js
+++ src/component/home/productList.js
@@ -1,11 +1,11 @@
 import Product1 from "./../../views/Home/homeImg/Hydrating Facial Cleanser.jpg"
 import Product2 from "./../../views/Home/homeImg/Revitalizing Night Cream.jpg"
 import Product3 from "./../../views/Home/homeImg/Vitamin C Serum.jpg"
-import Product4 from "./../../views/Home/homeImg/Long-Wear Liquid Foundation.png"
+import Product4 from "./../../views/Home/homeImg/Long-Wear Liquid Foundation.jpg"
 import Product5 from "./../../views/Home/homeImg/Matte Lipstick.jpg"
 import Product6 from "./../../views/Home/homeImg/Volumizing Mascara.jpg"
-import Product7 from "./../../views/Home/homeImg/Eyeshadow Palette.avif"
-import Product8 from "./../../views/Home/homeImg/Moisturizing Shampoo.png"
+import Product7 from "./../../views/Home/homeImg/eye_shadow_palette.png"
+import Product8 from "./../../views/Home/homeImg/Moisturizing Shampoo.jpg"
 import Product9 from "./../../views/Home/homeImg/Conditioning Hair Mask.jpg"
 import Product10 from "./../../views/Home/homeImg/Anti-Frizz Serum.jpg"
 
@@ -13,70 +13,70 @@ const ProductList = [
     
         {
             id:1,
-            productImg: {Product1},
+            productImg: Product1,
             rating: "4.5",
             productName: "Hydrating Facial Cleanser",
             productPrice: "$15.99"
         },
         {
             id:2,
-            productImg: {Product2},
+            productImg: Product2,
             rating: "4.7",
             productName: "Revitalizing Night Cream",
             productPrice: "$22.49"
         },
         {
             id:3,
-            productImg: {Product3},
+            productImg: Product3,
             rating: "4.6",
             productName: "Vitamin C Serum",
             productPrice: "$19.99"
         },
         {
             id:4,
-            productImg: {Product4},
+            productImg: Product4,
             rating: "4.8",
             productName: "Long-Wear Liquid Foundation",
             productPrice: "$29.99"
         },
         {
             id:5,
-            productImg: {Product5},
+            productImg: Product5,
             rating: "4.4",
             productName: "Matte Lipstick",
             productPrice: "$12.99"
         },
         {
             id:6,
-            productImg: {Product6},
+            productImg: Product6,
             rating: "4.6",
             productName: "Volumizing Mascara",
             productPrice: "$14.99"
         },
         {
             id:7,
-            productImg: {Product7},
+            productImg: Product7,
             rating: "4.5",
             productName: "Eyeshadow Palette",
             productPrice: "$24.99"
         },
         { 
             id:8,
-            productImg: {Product8},
+            productImg: Product8,
             rating: "4.7",
             productName: "Moisturizing Shampoo",
             productPrice: "$10.99"
         },
         {
             id:9,
-            productImg: {Product9}, 
+            productImg: Product9, 
             rating: "4.8",
             productName: "Conditioning Hair Mask",
             productPrice: "$17.49"
         },
         {
             id:10,
-            productImg: {Product10},
+            productImg: Product10,
             rating: "4.6",
             productName: "Anti-Frizz Serum",
             productPrice: "$13.99"
