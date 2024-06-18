2ce2af6133a660b5f2007b6399687d4c1432c667
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-06-18 12:05:15 +0530

    sign up p-age added

---
+++ src/views/Signup/Signup.js
@@ -0,0 +1,50 @@
+import "./Signup.css"
+import gmail from "./signup-icon/gmail.png"
+import facebook from "./signup-icon/facebook.png"
+import twitter from "./signup-icon/twitter.png"
+import signupimage from "./signup-icon/banner3-img.png"
+
+function Signup() {
+    return (
+    <>
+
+        <div className="sign-in-container">
+
+            <img src={signupimage} className="sign-in-tiger-img" />
+
+            <div className="sign-in-form-container">
+
+                <h1 className="sign-in-text">Sign in</h1>
+
+                <div className="sign-in-logo-img-container">
+                    <img src={gmail} className="sign-logo-img" />
+                    <img src={facebook} className="sign-logo-img" />
+                    <img src={twitter} className="sign-logo-img" />
+                </div>
+
+                <div className="sign-form-container">
+                    <form>
+
+                        <input type="text" placeholder="Name" className="input-box" />
+
+
+                        <input type="email" placeholder="Email" className="input-box" />
+
+                        <input type="password" placeholder="password" className="input-box" />
+
+
+                    </form>
+                </div>
+
+                <div>
+                    <button className="sign-in-page-btn">Sign in</button>
+                </div>
+            </div>
+        </div>
+
+    </>
+
+    )
+}
+
+export default Signup
