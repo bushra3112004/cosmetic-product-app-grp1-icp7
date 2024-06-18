a064d512132e24fc56375f1d0b04f521d274153d
Miss-Shubhangi <shubhangi.s.pawar2007@gmail.com>
2024-06-18 12:16:10 +0530

    testimonialData modified

--- src/component/home/testimonialData.js
+++ src/component/home/testimonialData.js
@@ -1,6 +1,6 @@
 const testimonials = [
     {
-      quote: "I've been using this cosmetic product for a few weeks now, and I'm absolutely thrilled with the results. My skin feels smoother, more hydrated, and has a radiant glow that I never thought possible. The lightweight formula absorbs quickly without any greasy residue, making it perfect for daily use. I highly recommend it to anyone looking to elevate their skincare routine.",
+      quote: "I've been using this cosmetic product for a few weeks now, and I'm absolutely thrilled with the results. My skin feels smoother, more hydrated, and has a radiant glow that I never thought possible.  I highly recommend it to anyone looking to elevate their skincare routine.",
       name: "Joe",
     },
     {
