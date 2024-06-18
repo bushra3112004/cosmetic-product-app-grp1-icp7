27c730741165339f230bc65a1004ddc85d6c0ea1
Sdsurve <sdsurve55555@gmail.com>
2024-06-18 15:12:06 +0530

    make responsive

--- src/component/ProductCards/ProductCards.js
+++ src/component/ProductCards/ProductCards.js
@@ -1,26 +1,53 @@
 import './ProductCards.css';
 import { Link } from 'react-router-dom';
-import { Toaster,toast } from "react-hot-toast"
-import ButtonBox from "./../../component/ButtonBox/ButtonBox"
+import { Toaster, toast } from "react-hot-toast";
+import ButtonBox from "./../../component/ButtonBox/ButtonBox";
 
-function ProductCards({ id, ProductImg, Title, Price,Categories }) {
-  return (<>
-    <Toaster />
-      <Link className="ProductCard" onClick={() => toast.success('Page is loded Succesfully')} to={`/Product/Productview/${id}`}>
+function ProductCards({ id, ProductImg, Title, Price, Categories }) {
+  const getCategoryColor = (category) => {
+    switch (category.toLowerCase()) {
+      case 'makeup':
+        return '#FFC0CB'; // Pink
+      case 'fashion':
+        return '#99ffff'; // Orange
+      case 'body & bath':
+        return '#ffccb3'; // Blue
+      case 'skin':
+        return '#ff8080'; // Green
+      case 'hair':
+        return '#FFD700'; // Gold
+      default:
+        return '#607D8B'; // Default Grey
+    }
+  };
+
+  return (
+    <>
+      <Toaster />
+      <Link
+        className="ProductCard"
+        onClick={() => toast.success('Page is loaded successfully')}
+        to={`/Product/Productview/${id}`}
+      >
         <div className="image">
-          <img   className="Productimg img-fluid" src={ProductImg} alt={Title} />
+          <img className="Productimg img-fluid" src={ProductImg} alt={Title} />
         </div>
-        <br/>
+        <br />
         <div className="info">
-          
-          <h3 className='cards-title fs-6'>{Title.length > 51 ? `${Title.substring(0, 51)}...` : Title}</h3>
+          <h3 className='cards-title fs-6'>
+            {Title.length > 51 ? `${Title.substring(0, 51)}...` : Title}
+          </h3>
           <p>Price: {Price}</p>
-          <ButtonBox className="btn-box" text="View Details" /><br/>
-          <button className='tags'>{Categories}</button>
+          <span className="btn-box"><ButtonBox  text="View Details" /></span><br />
+          <button
+            className='tags'
+            style={{ backgroundColor: getCategoryColor(Categories) }}
+          >
+            {Categories}
+          </button>
         </div>
       </Link>
-      </>
-
+    </>
   );
 }
 
