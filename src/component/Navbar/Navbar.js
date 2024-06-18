2afd69318626d483e3ea0e0b2358751ff4bb1ca7
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-06-18 11:24:54 +0530

    menu icon added in navbar

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,16 +1,33 @@
 
 import './Navbar.css';
 import { Link } from 'react-router-dom';
-import logo from "./logo-black.png"
+import logo from "./logo-black.png";
+import menuicon from "./menu.png";
+import { useState, useEffect } from 'react';
 
 function Navbar() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+
+
   return (<>
-    <div className='navbar-container' >
+    <div className={"navbar-container " } >
    
         <img src={logo} className='logo'/>
+
+        <div className='menu-icon'>
+          <img src={menuicon} className='menuicon'/>
+
+          </div>
        
         
-        <div className='nav-item-container '>
+        <div className={'nav-item-container '}>
          <Link to="/" className='nav-item'>Home</Link>
          <Link to="/product"  className='nav-item'>Products</Link>
          <Link to="/about"className='nav-item'>About</Link>
