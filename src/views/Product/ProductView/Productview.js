90fd028ae7efd27eb4a41706ca3be8ab7a6568ba
Sdsurve <sdsurve55555@gmail.com>
2024-06-18 10:38:04 +0530

    add related products

--- src/views/Product/ProductView/Productview.js
+++ src/views/Product/ProductView/Productview.js
@@ -6,6 +6,7 @@ import ProductCards from '../../../component/ProductCards/ProductCards';
 import Navbar from '../../../component/Navbar/Navbar';
 import Star from './star.png'
 import Back from './back.png'
+
 function Productview() {
   const { id } = useParams();
   const selectCard = Productdata.find((cardObj) => cardObj.id === id);
@@ -14,44 +15,46 @@ function Productview() {
     return <div>Product not found</div>;
   }
 
+  const relatedProducts = Productdata.filter((cardObj) => 
+    cardObj.Categories === selectCard.Categories && cardObj.id !== id
+  );
+
   return (
-    <><Navbar/>
-    <Link to='/Product' className='back-btn'><img height={'50px'} src={Back}/></Link>
+    <>
+      <Navbar/>
+      <Link to='/Product' className='back-btn'><img height={'50px'} src={Back} alt="Back"/></Link>
       <div className='main-view-container'>
         <div className='product-img-container'>
           <img className='product-img' src={selectCard.ProductImg} alt={selectCard.name} />
         </div>
         <div className='Dis-container'>
-
           <span className='title'>{selectCard.Title}</span>
           <br />
           <br />
-          <p>Price:- {selectCard.Price}</p>
-          <p>Rating:- {selectCard.Rating}/5 <img className='star-img' src={Star}/></p>
-          <p>Reviews:- {selectCard.Reviews}</p>
-          <p>Category:- {selectCard.Categories}</p>
-          <p>Sold By:- {selectCard.Brand}</p>
+          <p>Price: {selectCard.Price}</p>
+          <p>Rating: {selectCard.Rating}/5 <img className='star-img' src={Star} alt="Star"/></p>
+          <p>Reviews: {selectCard.Reviews}</p>
+          <p>Category: {selectCard.Categories}</p>
+          <p>Sold By: {selectCard.Brand}</p>
           <p className='discription'>Product Description: {selectCard.Description}</p>
-
         </div>
       </div>
       <div className='use-discription-container'>
         <div className='Ingrediants-container'>
-          <span className='Ingridiant-title'>Ingredients :-</span>
-       <br/><br/><br/>
-       <p>{selectCard.Ingredients.Ingredient1}</p>
-       <p>{selectCard.Ingredients.Ingredient2}</p>
-       <p>{selectCard.Ingredients.Ingredient3}</p>
-       <p>{selectCard.Ingredients.Ingredient4}</p>
-       <p>{selectCard.Ingredients.Ingredient5}</p>
-       <p>{selectCard.Ingredients.Ingredient6}</p>
-       <p>{selectCard.Ingredients.Ingredient7}</p>
-       <p>{selectCard.Ingredients.Ingredient8}</p>
+          <span className='Ingridiant-title'>Ingredients:</span>
+          <br/><br/><br/>
+          <p>{selectCard.Ingredients.Ingredient1}</p>
+          <p>{selectCard.Ingredients.Ingredient2}</p>
+          <p>{selectCard.Ingredients.Ingredient3}</p>
+          <p>{selectCard.Ingredients.Ingredient4}</p>
+          <p>{selectCard.Ingredients.Ingredient5}</p>
+          <p>{selectCard.Ingredients.Ingredient6}</p>
+          <p>{selectCard.Ingredients.Ingredient7}</p>
+          <p>{selectCard.Ingredients.Ingredient8}</p>
         </div>
         <div className='Uses-container'> 
-          <span className='Uses-heading'>Uses :-</span>
-          <br/><br/>
-          <br/>
+          <span className='Uses-heading'>Uses:</span>
+          <br/><br/><br/>
           <p>{selectCard.Use.Use1}</p>
           <p>{selectCard.Use.Use2}</p>
           <p>{selectCard.Use.Use3}</p>
@@ -62,36 +65,35 @@ function Productview() {
         </div>
       </div>
       <div>
-        <div><span className='Related-content-heading'>How To Use ?</span></div>
+        <div><span className='Related-content-heading'>How To Use?</span></div>
         <br/>
         <div className='howtouse-containt'>
-        <p>{selectCard.HowToUse.HowToUse1}</p>
-        <p>{selectCard.HowToUse.HowToUse2}</p>
-        <p>{selectCard.HowToUse.HowToUse3}</p>
+          <p>{selectCard.HowToUse.HowToUse1}</p>
+          <p>{selectCard.HowToUse.HowToUse2}</p>
+          <p>{selectCard.HowToUse.HowToUse3}</p>
         </div>
-
       </div>
       <span className='Related-content-heading'>Related Products</span>
       <div className='pcardsContainers2'>
-
         {
-          Productdata.map((Carddata, i) => {
+          relatedProducts.map((Carddata) => {
             const {
               id,
               ProductImg,
               Title,
               Price,
               Categories,
-            } = Carddata
+            } = Carddata;
             return (
               <ProductCards
-                key={3}
+                key={id}
                 id={id}
                 ProductImg={ProductImg}
                 Title={Title}
                 Price={Price}
                 Categories={Categories}
-              />)
+              />
+            );
           })
         }
       </div>
